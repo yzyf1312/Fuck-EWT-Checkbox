c6a015f9e2bd4c5d7508e19321fc81342a721688
yzyf1312 <yzyf1312@outlook.com>
2024-01-30 09:44:37 +0800

    Update fuck-check-box.js

--- fuck-check-box.js
+++ fuck-check-box.js
@@ -14,19 +14,22 @@ var intervalId;
 var clickCount = 0;
 
 function clickElementPeriodically() {
-  var element = document.querySelector('.earnest_check_mask_box');
-  if (element) {
-    clearInterval(intervalId); // 清除之前设置的定时器
-    intervalId = setInterval(function() {
-      element.click();
-      clickCount++;
-      if (clickCount >= 2 || !document.contains(element)) {
-        clearInterval(intervalId); // 点击2次或元素消失后清除定时器
-      }
-    }, 2000);
-  } else {
-    clearInterval(intervalId); // 如果元素不存在，则清除定时器
-  }
+  var observer = new MutationObserver(function(mutationsList, observer) {
+    var element = document.querySelector('.earnest_check_mask_box');
+    if (element) {
+      clearInterval(intervalId); // 清除之前设置的定时器
+      intervalId = setInterval(function() {
+        element.click();
+        clickCount++;
+        if (clickCount >= 2 || !document.contains(element)) {
+          clearInterval(intervalId); // 点击2次或元素消失后清除定时器
+        }
+      }, 2000);
+      observer.disconnect(); // 元素出现后，停止监听
+    }
+  });
+
+  observer.observe(document, { subtree: true, childList: true }); // 开始监听DOM变化
 }
 
 clickElementPeriodically();
